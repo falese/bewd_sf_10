9d4f0ac9214638498f177401605fde15c27a57d5
falese <falese@gmail.com>
2015-08-10 21:26:56 -0700

    commit of car_lot.rb

--- 04_Ruby_Fundamentals/exercises/car_lot.rb
+++ 04_Ruby_Fundamentals/exercises/car_lot.rb
@@ -11,24 +11,27 @@ def show_all_cars(hashes)
   hashes.each do |hash|
     hash
     puts "The model is #{hash[:model]} and the drive train is #{hash[:drivetrain]}"
+      puts "its electric boogie woogie woogie" unless hash[:is_electric] == false
+    end
 
-  end
 
 
 end
 
 #write a method that accepts an unlimited number of cars (or arguments)
-def add_cars()
+def add_cars(*all_cars)
+  hashes
+
+
 
 end
 h1 = {model: "s", drivetrain:"AWD", seats: "4", body_style:"sedan" ,is_electric: true}
 h2 = {model: "prius", drivetrain:"FWD", seats: "4", body_style: "econobox", is_electric: true}
 h3 = {model: "jeep", drivetrain:"4WD", seats: "5", body_stlye: "SUV", is_electric: false}
 h4 = {model: "corolla", drivetrain: "FWD", seats: "4", body_style:"econobox", is_electric: false}
-hashes = []
-hashes.push(h1,h2,h3,h4)
+# hashes = []
+# hashes.push(h1,h2,h3,h4)
 
 
-puts hashes
+hashes = add_cars(h1, h2, h3)
 show_all_cars(hashes)
-puts "hello"
