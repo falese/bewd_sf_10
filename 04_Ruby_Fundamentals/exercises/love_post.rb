7e8144f9a87fafce106b9a7537bab3e8b9fbb356
falese <falese@gmail.com>
2015-08-10 20:11:59 -0700

    commit of love_post.rb

--- 04_Ruby_Fundamentals/exercises/love_post.rb
+++ 04_Ruby_Fundamentals/exercises/love_post.rb
@@ -4,22 +4,27 @@ require 'pry'
 def get_love_interest
   puts "who do ya love?"
   var1 = gets.downcase.chomp
-  puts "now tell me do ya love #{var1} forever?"
+  capture_love_interest_response(var1)
+end
+def capture_love_interest_response(var1)
+  puts "now tell me do ya love #{var1} forever? \n Oh Oh Oh"
   var2 = gets.downcase.chomp
-  if var2 == "yes"
-    puts "oh oh oh, go paula abdul"
-  else
-    puts "simon cowell doesn't like you"
+  # binding.pry
+  get_valid_answer(var2, var1)
 end
 
+def get_valid_answer(var2, var1)
 
+  case var2.downcase
+  when "yes"
+   "Oh oh oh Paula Abdul! You are very lucky to love #{var1}"
+  when "no"
+   "Meatloaf for you buddy"
+ else
+    puts "Please try again 'yes' or 'no' answers only"
+    capture_love_interest_response(var1)
+  end
 end
 
-def capture_love_interest_response(love_interest)
-end
-
-def get_valid_answer(user_answer, love_interest)
-end
-
-### This is where we call methods to run our script
-get_love_interest
+### This is where we call methods to run our script and output its result
+puts get_love_interest
